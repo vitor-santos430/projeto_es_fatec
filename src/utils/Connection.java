1d0d19e9fa24a5a928865407511b728b9a0673fa
vitor-santos430 <vitordesouza230702@gmail.com>
2022-04-09 02:07:49 -0300

    Inicio classe de conexão com banco de dados

---
+++ src/utils/Connection.java
@@ -0,0 +1,21 @@
+package utils;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
+
+public class Connection {
+	public Connection conectaDB(){
+		Connection conn = null;
+		
+		try {
+			String url = "jdbc:mysql://localhost:3306/db_sistema_clube?user=root&password=";
+			conn = (Connection)DriverManager.getConnection(url);
+		}catch(SQLException e) {
+			JOptionPane.showMessageDialog(null, e.getMessage());
+		}
+		
+		return conn;
+	}
+}
