7b4872de547180863dc0302ed5235d2b06c560fc
devkemc <joaquimkbs@gmail.com>
2022-05-09 21:45:04 -0300

    nova versao v00.35

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,25 @@
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+import javax.swing.JOptionPane;
+
+import java.sql.Connection;
 
 public class Main {
 
 	public static void main(String[] args) {
+		String user = "root";
+		String senha = "";
 		
+		Connection conn = null;
 		
-		
+		try {
+			String url = "jdbc:mysql://localhost:3306/db_sistema_clube";
+			conn = DriverManager.getConnection(url,user, senha);
+			JOptionPane.showMessageDialog(null,"Banco conectado");
+		}catch(SQLException e) {
+			JOptionPane.showMessageDialog(null, e.getMessage());
+		}
 		
 	}
 
